dab88e4b067c32611ba93a8dd95f4b7e53cf3b6f
kmotohas <kazuki.motohashi@icloud.com>
2019-11-13 19:42:04 +0900

    add a part of scdf exapmle

---
+++ spring-projects/usage-cost-stream-rabbit/usage-detail-sender-rabbit/src/test/java/io/spring/dataflow/sample/usagedetailsenderrabbit/UsageDetailSenderRabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.dataflow.sample.usagedetailsenderrabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsageDetailSenderRabbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
