2e191ec8f82131b56fe1a0bc826497266a895926
kmotohas <kazuki.motohashi@icloud.com>
2019-12-12 14:12:15 +0900

    add scdf tutorial

---
+++ spring-projects/usage-cost-stream-rabbit-0.0.1-SNAPSHOT/usage-cost-logger/src/test/java/io/spring/dataflow/sample/usagecostlogger/UsageCostLoggerApplicationTests.java
@@ -0,0 +1,52 @@
+package io.spring.dataflow.sample.usagecostlogger;
+
+import io.spring.dataflow.sample.domain.UsageCostDetail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.cloud.stream.messaging.Sink;
+
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UsageCostLoggerApplicationTests {
+
+	@Autowired
+	private Sink sink;
+
+	@Autowired
+	private UsageCostLogger usageCostLogger;
+
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void testUsageCostLogget() throws Exception {
+		ArgumentCaptor<UsageCostDetail> captor = ArgumentCaptor.forClass(UsageCostDetail.class);
+		this.sink.input().send(MessageBuilder.withPayload(("{\"userId\":\"user3\",\"callCost\":10.100000000000001,\"dataCost\":25.1}")).build());
+		verify(this.usageCostLogger).process(captor.capture());
+	}
+
+	@EnableAutoConfiguration
+	@EnableBinding(Sink.class)
+	static class TestConfig {
+		@Bean
+		@Primary
+		public UsageCostLogger usageConstLogget() {
+			return spy(new UsageCostLogger());
+		}
+	}
+
+}
